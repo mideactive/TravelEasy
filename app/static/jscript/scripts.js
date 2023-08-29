52d43de364787b1f148ba5ac0e632717feb667b1
mideactive <jenyoolumide@yahoo.com>
2023-08-29 01:33:49 +0000

    static files updated

---
+++ app/static/jscript/scripts.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+        const getStartedButton = document.getElementById("get-started-button");
+        const signupForm = document.getElementById("signup-form");
+        let formVisible = false;
+
+        // Toggle form visibility when Get Started button is clicked
+        getStartedButton.addEventListener("click", function () {
+            if (!formVisible) {
+                signupForm.style.display = "block";
+            } else {
+                signupForm.style.display = "none";
+            }
+            formVisible = !formVisible;
+        });
+    });
+function submitLoginForm(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        // Get the form data
+        const formData = new FormData(event.target);
+
+        // Send a POST request to the Flask login route
+        fetch('/login', {
+            method: 'POST',
+            body: formData,
+            redirect: 'follow' // Follow redirects
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url; // Redirect to the returned URL
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
